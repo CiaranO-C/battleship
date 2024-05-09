f105f0a3b599df95803c2bc39702535fa02dd433
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-05-09 21:56:01 +0100

    add score property to player factory
    write basic structure of html doc
    
    create file for DOM module

--- player.js
+++ player.js
@@ -3,6 +3,7 @@ import { Board } from "./gameboard.js";
 function Player(name = "Computer") {
   let title = name;
   let board = Board();
+  let score = 0;
 
   function getName() {
     return name;

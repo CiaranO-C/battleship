331448d9ab0ede1bd4d479cd0fe2e9591858698f
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-20 10:53:31 +0000

    write more extensive gameboard tests

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -9,37 +9,108 @@ const shipsData = [
   ["Patrol", 2],
 ];
 
-const testBoard = Board();
-const battleship = Ship(shipsData[1][0], shipsData[1][1]);
+describe("Test Gameboard methods", () => {
+  const boardSize = 10;
+  let testBoard;
+  let battleship;
+  let i;
+  let j;
 
-test("Test random index numbers 0-9", () => {
-  const [i, j] = [0, 0];
-  expect(testBoard.placeShip(battleship, i, j)).toBeTruthy();
-  //ship of length 3 should span from 0,0 to 0,2
-  expect(testBoard.getCell(i, j).getShip()).toBe(battleship);
-  expect(testBoard.getCell(i, j + 1).getShip()).toBe(battleship);
-  expect(testBoard.getCell(i, j + 2).getShip()).toBe(battleship);
-});
+  beforeEach(() => {
+    testBoard = Board();
+    battleship = Ship(shipsData[1][0], shipsData[1][1]);
+    [i, j] = [0, 0];
+  });
 
-test("recieves attack and updates ship or misses", () => {
-  const [i, j] = [0, 0];
-  expect(testBoard.recieveAttack(i, j)).toBeTruthy();
-  expect(battleship.hitsTaken()).toEqual(1);
+  test("getBoard returns board array", () => {
+    const board = testBoard.getBoard();
+    expect(Array.isArray(board)).toBe(true);
+    expect(board.length).toBe(boardSize);
+  });
 
-  expect(testBoard.recieveAttack(i, j)).toBeFalsy();
-  expect(battleship.hitsTaken()).toEqual(1);
-});
+  test("randomises ship positions", () => {
+    function getPositions(boardArr) {
+      //converts to a board of true/false based on ship presence
+      const boolBoard = boardArr.map((row) =>
+        row.map((cell) => cell.hasShip()),
+      );
+      let positions = "";
+      //adds any cell with true to positions string
+      boolBoard.forEach((row, i) =>
+        row.forEach((cell, j) =>
+          cell ? (positions += `[${i},${j}] `) : undefined,
+        ),
+      );
 
-test("gameboard checks if all ships have sunk", () => {
-  const newBoard = Board();
-  newBoard.randomize();
-  const totalShipCells = 17;
-  expect(newBoard.shipsSunk()).toBeFalsy();
-  //hits entire board to test if ships sunk
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < 10; j++) {
-      newBoard.recieveAttack(i, j);
+      return positions;
     }
-  }
-  expect(newBoard.shipsSunk()).toBeTruthy();
+
+    testBoard.randomize();
+    const posOne = getPositions(testBoard.getBoard());
+
+    testBoard.randomize();
+    const posTwo = getPositions(testBoard.getBoard());
+    expect(posOne).not.toEqual(posTwo);
+  });
+
+  test("Places ship, or returns false for invalid position", () => {
+    expect(testBoard.placeShip(battleship, i, j)).toBe(true);
+    expect(testBoard.placeShip(battleship, i, j)).toBe(false);
+  });
+
+  test("Places ship correctly, and get cell contents", () => {
+    expect(testBoard.placeShip(battleship, i, j)).toBeTruthy();
+    //ship of length 3 should span from 0,0 to 0,2
+    expect(testBoard.getCell(i, j).getShip()).toBe(battleship);
+    expect(testBoard.getCell(i, j + 1).getShip()).toBe(battleship);
+    expect(testBoard.getCell(i, j + 2).getShip()).toBe(battleship);
+    expect(testBoard.getCell(9, 9).getShip()).toBeNull();
+  });
+
+  test("Attack on same coordinates does not update hits twice", () => {
+    testBoard.placeShip(battleship, i, j);
+    expect(testBoard.recieveAttack(i, j)).toBeTruthy();
+    expect(battleship.hitsTaken()).toEqual(1);
+
+    expect(testBoard.recieveAttack(i, j)).toBeFalsy();
+    expect(battleship.hitsTaken()).toEqual(1);
+  });
+
+  test("Checks if all ships have sunk", () => {
+    testBoard.randomize();
+    expect(testBoard.shipsSunk()).toBeFalsy();
+    //hits entire board to test if ships sunk
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        testBoard.recieveAttack(i, j);
+      }
+    }
+    expect(testBoard.shipsSunk()).toBeTruthy();
+  });
+
+  test("resetBoard sets total hits to 0", () => {
+    testBoard.randomize();
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        testBoard.recieveAttack(i, j);
+      }
+    }
+    expect(testBoard.shipsSunk()).toBe(true);
+    testBoard.resetBoard();
+    expect(testBoard.shipsSunk()).toBe(false);
+  });
+
+  test("Validates targets, true if good, false otherwise", () => {
+    expect(testBoard.validateAttack(testBoard.getCell(i, j))).toBe(true);
+    testBoard.recieveAttack(i, j);
+    expect(testBoard.validateAttack(testBoard.getCell(i, j))).toBe(false);
+  });
+
+  test("Can create ships", () => {
+    const [carrierName, carrierLength] = shipsData[0];
+    const carrier = testBoard.createShip(carrierName, carrierLength);
+    expect(carrier).toBeTruthy();
+    expect(carrier.getName()).toEqual(carrierName);
+    expect(carrier.getLength()).toEqual(carrierLength);
+  });
 });

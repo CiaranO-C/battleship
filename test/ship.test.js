adb402283fd3bc69d805cc16f9a7121087360a90
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2025-01-20 11:37:25 +0000

    write further ship method tests

--- test/ship.test.js
+++ test/ship.test.js
@@ -1,8 +1,13 @@
-import { Ship } from '../src/gameLogic/ship';
+import { Ship } from "../src/gameLogic/ship";
 
 describe("test ship object methods", () => {
-    const shipLength = 5;
-    const testShip = Ship("testShip", shipLength);
+  let testShip;
+  const shipLength = 5;
+  const shipName = "testShip";
+  beforeEach(() => {
+    testShip = Ship(shipName, shipLength);
+  });
+
   test("Hit function increments hit counter", () => {
     testShip.hit();
     expect(testShip.hitsTaken()).toBe(1);
@@ -12,9 +17,29 @@ describe("test ship object methods", () => {
 
   test("Check if ship is sunk when hits === length", () => {
     expect(testShip.isSunk()).toBe(false);
-    for(let i = 0; i<3; i++){
-        testShip.hit();
-    };
+    for (let i = 0; i < shipLength; i++) {
+      testShip.hit();
+    }
     expect(testShip.isSunk()).toBe(true);
   });
+
+  test("testShip stores its name and length", () => {
+    expect(testShip.getName()).toEqual(shipName);
+    expect(testShip.getLength()).toEqual(shipLength);
+  });
+
+  test("No axis argument passed creates horizontal ship", () => {
+    expect(testShip.isVertical()).toBe(false);
+  })
+
+  test("Axis argument passed creates vertical ship", () => {
+    const vShip = Ship(shipName, shipLength, true);
+    expect(vShip.isVertical()).toBe(true);
+  })
+
+  test("Rotate toggles ship axis", () => {
+    expect(testShip.isVertical()).toBe(false);
+    testShip.rotate()
+    expect(testShip.isVertical()).toBe(true);
+  })
 });
